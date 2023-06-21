2a36a5cdc27f518fa558cef1503bb8990b70a7e3
Phung Nguyen Huu <phung@swiftpod.com>
2023-06-21 18:04:01 +0700

    QR SCAN

--- src/modules/auth/views/Index.vue
+++ src/modules/auth/views/Index.vue
@@ -3,6 +3,8 @@
     <ScanQRCode
       title="Scan Employee Code to Login"
       subtitle="Align the QR code within the frame to scan"
+      @result="onScan"
+      @error="onError"
     >
       <template #default>
         <s-button outline class="!bg-white active:!opacity-80" @click="router.go(-1)">
@@ -16,8 +18,49 @@
 <script setup lang="ts">
 import ScanQRCode from '@/components/ScanQRCode.vue';
 import { useRouter } from 'vue-router';
+import { soundScan } from '@/utils/helper';
+import type {
+  QrcodeResult,
+  Html5QrcodeError,
+  CameraDevice,
+  Html5QrcodeCameraScanConfig,
+} from 'html5-qrcode';
+import EventBus from '@/utils/eventbus';
 
 const router = useRouter();
+
+const onScan = (decodedText: string, decodedResult: QrcodeResult) => {
+  console.log(decodedResult);
+  if (decodedText) {
+    let isValidJSON = true;
+    let result = {};
+
+    try {
+      result = JSON.parse(decodedText);
+    } catch (error) {
+      isValidJSON = false;
+    }
+
+    if (
+      isValidJSON &&
+      Object.keys(result).includes('employee_code') &&
+      Object.keys(result).includes('department') &&
+      Object.keys(result).length === 2
+    ) {
+      EventBus.$emit('changeState', 3);
+      soundScan();
+
+      // const payload = {
+      //   ...result,
+      //   token: notificationStore.firebaseToken,
+      // }
+
+      //TODO Handle Login
+
+      router.push('/home');
+    }
+  }
+};
 </script>
 
 <style scoped></style>
