53c4d9ce9cbc4b26535a81ee5820817a35fbb1cb
Phung Nguyen Huu <phung@swiftpod.com>
2023-06-23 13:38:16 +0700

    update

--- src/register-sw.ts
+++ src/register-sw.ts
@@ -1,18 +1,20 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
-  window.addEventListener('load', () => {
-    console.log(navigator.serviceWorker);
-    navigator.serviceWorker
-      .register('/src/services/firebase-messaging-sw.ts')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
+(() => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+    window.addEventListener('load', () => {
+      console.log(navigator.serviceWorker);
+      navigator.serviceWorker
+        .register('@/services/firebase-messaging-sw.ts')
+        .then((registration) => {
+          console.log('Service Worker registered:', registration);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+})()
