b8c5e70156842992312df25d2e972de2f78973a3
Phung Nguyen <phung@swiftpod.com>
2023-06-29 17:14:31 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
       </div>
     </div>
   </Teleport>
-  <!-- <Teleport to="body">
+  <Teleport to="body">
     <div
       v-if="!checkAppInstalled()"
       class="wraper !z-[9999] bg-[rgba(10,11,13,0.3)] backdrop-blur-[12px] absolute top-0 left-0 w-screen h-screen text-white flex flex-col justify-end"
@@ -51,7 +51,7 @@
         <img src="@/assets/images/logo.png" alt="icon" class="h-full" />
       </div>
     </div>
-  </Teleport> -->
+  </Teleport>
 </template>
 
 <script setup lang="ts">
@@ -60,13 +60,13 @@ import { useRoute } from 'vue-router';
 import MainLayout from '@/layouts/MainLayout.vue';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import NewNotifyMessage from '@/components/NewNotifyMessage.vue';
-import { useSocket } from '@/services/socket';
+// import { useSocket } from '@/services/socket';
 import { checkAppInstalled } from '@/utils/device';
 import { useNotificationStore } from '@/stores/notification';
 import { getToken } from 'firebase/messaging';
 import { messaging } from '@/utils/firebase';
 
-const socket = useSocket();
+// const socket = useSocket();
 const route = useRoute();
 const notificationStore = useNotificationStore();
 
@@ -116,7 +116,7 @@ const refreshFCMToken = async () => {
 };
 
 onMounted(() => {
-  socket.asyncConnect();
+  // socket.asyncConnect();
   refreshFCMToken();
   if (Notification.permission !== 'granted') {
     local.showPopupEnableNotify = true;
